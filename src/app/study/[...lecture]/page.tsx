d9bb542fca6c1490c53a14ae0da9e3ae4500f040
Alla-ud-din <allauddin.aliahmad9@gmail.com>
2023-12-24 12:29:39 +0500

    updating file

--- src/app/study/[...lecture]/page.tsx
+++ src/app/study/[...lecture]/page.tsx
@@ -3,7 +3,7 @@ export default function Lecture({params}: any){
     console.log(params);
     return(
         <div>
-            <h1> Day of college {params.lecture[0]} </h1>
+            <h1>Day of college {params.lecture[0]} </h1>
             <h2>Lecture No. {params.lecture[1]} </h2>
             <h3>Credit Hrs. {params.lecture[2]}</h3>
         </div>
