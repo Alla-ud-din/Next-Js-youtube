20801137e28b78f9b2d4bddd09375b429933bff9
Alla-ud-din <allauddin.aliahmad9@gmail.com>
2024-02-09 15:28:50 +0500

    updating files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
+import { Roboto } from "next/font/google"
+const roboto= Roboto({
+  weight:'100',
+  subsets:['latin'],
+  display:'swap'
+})
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Basic Routing | Make New Page</h1>
+    <main>
+      <h1>Font Optimization in Next</h1>
+      {/* <h1 style={{fontFamily:'Roboto', fontWeight:100}} >Font with Link Tag in Next</h1> */}
+    <h1 className={roboto.className}>Font with Next js font feature</h1>
     </main>
   )
 }
