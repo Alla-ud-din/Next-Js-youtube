41c1b72cb335646f6427258a2f86ad9a5031beec
Alla-ud-din <allauddin.aliahmad9@gmail.com>
2024-02-10 11:21:01 +0500

    updating branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Basic Routing | Make New Page</h1>
+      <h1>API with Static Data</h1>
     </main>
   )
 }
---
+++ src/util/db.ts
@@ -0,0 +1,20 @@
+export const user = [
+    {
+        id: 20,
+        name: "Ali",
+        age: 6,
+        email: "ali@test.com"
+    },
+    {
+        id: 21,
+        name: "Muazam",
+        age: 4,
+        email: "Muazam@test.com"
+    },
+    {
+        id: 22,
+        name: "Mukaram",
+        age: 1,
+        email: "Mukaram@test.com"
+    }
+]
